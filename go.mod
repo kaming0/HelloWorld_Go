310f157225fec165cff194a19513a5f36a97531a
kamingleung <kleung@rapiscansystems.com>
2021-07-14 08:45:35 +1000

    Added log package and better error handling

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/kaming0/HelloWorld_Go
 go 1.16
 
 require (
-	github.com/kaming0/greetings v0.0.0-20210713080308-913cc561b829
+	github.com/kaming0/greetings v0.0.0-20210713090115-4412a399d454
+	golang.org/x/text v0.3.6 // indirect
 	rsc.io/quote v1.5.2
+	rsc.io/sampler v1.99.99 // indirect
 )
--- hello.go
+++ hello.go
@@ -2,12 +2,19 @@ package main
 
 import (
     "fmt"
+	"log"
     "github.com/kaming0/greetings"
 )
 
 import "rsc.io/quote"
 
 func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
     fmt.Println("Hello, World!")
     fmt.Println(quote.Glass())
     fmt.Println(quote.Go())
@@ -15,6 +22,14 @@ func main() {
     fmt.Println(quote.Opt())
 
     // Use github.com/kaming0/greetings
-    message := greetings.Hello("Ka Ming")
+    message, err := greetings.Hello("") //("Ka Ming")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.	
     fmt.Println(message)
 }
