427b6c3b1fd6f6e6585b6750c95bfa8c793d1481
kamingleung <kleung@rapiscansystems.com>
2021-07-13 18:42:03 +1000

    Use greetings for peronalized messages

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kaming0/HelloWorld_Go
 
 go 1.16
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/kaming0/greetings v0.0.0-20210713080308-913cc561b829
+	rsc.io/quote v1.5.2
+)
--- hello.go
+++ hello.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/kaming0/greetings"
+)
 
 import "rsc.io/quote"
 
 func main() {
     fmt.Println("Hello, World!")
-	fmt.Println(quote.Glass())
+    fmt.Println(quote.Glass())
     fmt.Println(quote.Go())
-	fmt.Println(quote.Hello())
-	fmt.Println(quote.Opt())
+    fmt.Println(quote.Hello())
+    fmt.Println(quote.Opt())
+
+    // Use github.com/kaming0/greetings
+    message := greetings.Hello("Ka Ming")
+    fmt.Println(message)
 }
