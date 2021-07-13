ad4cff1d0b1c0a3a0853e8628a7c03a5fd1c6427
kamingleung <kleung@rapiscansystems.com>
2021-07-13 15:58:58 +1000

    Print more quote messages

--- hello.go
+++ hello.go
@@ -5,5 +5,9 @@ import "fmt"
 import "rsc.io/quote"
 
 func main() {
+    fmt.Println("Hello, World!")
+	fmt.Println(quote.Glass())
     fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
 }
